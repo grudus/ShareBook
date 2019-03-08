180c7b51586c251748c3242098fa6159d85f3812
grudus <jakgruda@gmail.com>
2019-03-08 11:03:37 +0100

    Initial commit

---
+++ backend/src/test/kotlin/com/pwr/sharebook/SharebookApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.pwr.sharebook
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SharebookApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
