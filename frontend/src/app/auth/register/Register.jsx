3cb565e0856a95325899a4024dfc3aacd7fee02b
Wiktoria <238039@student.pwr.edu.pl>
2019-03-24 18:51:09 +0100

    register-validation

--- frontend/src/app/auth/register/Register.jsx
+++ frontend/src/app/auth/register/Register.jsx
@@ -7,33 +7,97 @@ import { Link } from "react-router-dom";
 import React, { Component } from "react";
 import { register } from "../AuthApi";
 
+const formValid = ({formErrors, ...rest}) => {
+    let valid = true;
+
+    Object.values(formErrors).forEach(val => {
+        val.length > 0 && (valid = false);
+    });
+
+    Object.values(rest).forEach(val => {
+        val == null && (valid = false);
+    });
+
+    return valid;
+};
+
+const emailRegex = RegExp(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/);
 
 class Register extends Component {
 
-    state = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
-        conditions: false,
-    };
+    constructor(props){
+        super(props);
+
+        this.state = {
+            firstName: null,
+            lastName: null,
+            email: null,
+            password: null,
+            confirmPassword: null,
+            conditions: false,
+            formErrors:{
+                firstName: "",
+                lastName: "",
+                email: "",
+                password: "",
+                confirmPassword: "",
+                conditions: false,
+            }
+        };
+    }
+
 
     handleChange = (field) => (event) => {
         this.setState({ [field]: event.target.value });
     };
 
+    handleChange2 = (field) => (event) => {
+        event.preventDefault();
+        const{name, value} = event.target;
+        let formErrors = {...this.state.formErrors};
+
+        console.log("Name: ", name);
+        console.log("value:", value);
+
+        switch (name){
+            case "firstName":
+                formErrors.firstName = value.length < 1 ? "First name is required" : "";
+                break;
+            case "lastName":
+                formErrors.lastName = value.length < 1 ? "Last name is required" : "";
+                break;
+            case "email":
+                formErrors.email = emailRegex.test(value) ? "" : "Invalid email address";
+                break;
+            case "password":
+                formErrors.password = value.length < 6 && value.length > 0 ? "Minimum 6 characters required" : "";
+                break;
+            case "confirmPassword":
+                formErrors.confirmPassword = value.length < 6 ? "Minimum 6 characters required" : "";
+                break;
+            default:
+                break;
+        }
+        this.setState({formErrors, [name]: value}, () => console.log(this.state));
+    };
+
     submitForm = async (e) => {
         e.preventDefault();
-        try {
-            await register(this.state);
-            alert("Success")
-        } catch (e) {
-            alert("Invalid credentials")
+        if(formValid(this.state)){
+            console.log(`
+        --SUBMITTING--
+        First Name: ${this.state.firstName}
+        Last Name: ${this.state.lastName}
+        Email: ${this.state.email}
+        Password: ${this.state.password}
+      `);
+        } else{
+            console.error('FORM INVALID- DISPLAY ERROR MESSAGE');
         }
     };
 
     render() {
+        const{formErrors} = this.state;
         return (
             <div>
                 <Card className={css.registerCard}>
@@ -50,46 +114,70 @@ class Register extends Component {
                                 <div className={css.fullName1}>
                                     <TextField
                                         required
+                                        className={formErrors.firstName.length > 0 ? "error" : null}
                                         label="First Name"
-                                        value={this.state.firstName}
-                                        onChange={this.handleChange('firstName')}
-                                        margin="normal"
+                                        type="text"
+                                        name="firstName"
+                                        noValidate
+                                        onChange={this.handleChange2('firstName')}
                                     />
+                                    {formErrors.firstName.length > 0 && (
+                                        <span className={css.error}>{formErrors.firstName}</span>
+                                    )}
                                 </div>
                                 <div className={css.fullName2}>
                                     <TextField
                                         required
+                                        className={formErrors.lastName.length > 0 ? "error" : null}
                                         label="Last Name"
-                                        value={this.state.lastName}
-                                        onChange={this.handleChange('lastName')}
-                                        margin="normal"
-
+                                        type="text"
+                                        name="lastName"
+                                        noValidate
+                                        onChange={this.handleChange2('lastName')}
                                     />
+                                    {formErrors.lastName.length > 0 && (
+                                        <span className={css.error}>{formErrors.lastName}</span>
+                                    )}
                                 </div>
                             </div>
+                                <TextField
+                                    required
+                                    className={formErrors.email.length > 0 ? "error" : null}
+                                    label="Email"
+                                    type="email"
+                                    name="email"
+                                    noValidate
+                                    onChange={this.handleChange2('email')}
+                                />
+                                {formErrors.email.length > 0 && (
+                                    <span className={css.error}>{formErrors.email}</span>
+                                )}
                             <TextField
                                 required
-                                label="Email"
-                                value={this.state.email}
-                                onChange={this.handleChange('email')}
-                                margin="normal"
-                            />
-                            <TextField
-                                required
+                                className={formErrors.password.length > 6 ? "error" : null}
                                 label="Password"
-                                value={this.state.password}
-                                onChange={this.handleChange('password')}
+                                name="password"
+                                noValidate
+                                onChange={this.handleChange2('password')}
                                 type="password"
                                 margin="normal"
                             />
+                            {formErrors.password.length > 0 && (
+                                <span className={css.error}>{formErrors.password}</span>
+                            )}
                             <TextField
                                 required
                                 label="Confirm Password"
-                                value={this.state.confirmPassword}
-                                onChange={this.handleChange('confirmPassword')}
+                                className={formErrors.confirmPassword.length > 6 ? "error" : null}
+                                onChange={this.handleChange2('confirmPassword')}
                                 type="password"
+                                noValidate
+                                name="confirmPassword"
                                 margin="normal"
                             />
+                            {formErrors.confirmPassword.length > 0 && (
+                                <span className={css.error}>{formErrors.confirmPassword}</span>
+                            )}
                             <div className={css.conditionsWrapper}>
                                 <input type="checkbox" className={css.checkbox}/>
                                 <label>
