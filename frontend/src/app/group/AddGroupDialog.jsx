efc0bc7dff490c12d536f22901e25bdb70672a9a
Wiktoria <238039@student.pwr.edu.pl>
2019-04-01 19:41:18 +0200

    validation-GroupDialog

--- frontend/src/app/group/AddGroupDialog.jsx
+++ frontend/src/app/group/AddGroupDialog.jsx
@@ -5,6 +5,7 @@ import TextField from "@material-ui/core/TextField";
 import React, {Component, useState} from 'react';
 import css from './group.module.scss'
 import * as PropTypes from "prop-types";
+import {register} from "../auth/AuthApi";
 
 const defaultPhotoUrl = "https://3ie87c2dond928rt2e2zzo8o-wpengine.netdna-ssl.com/wp-content/themes/gonzo/images/no-image-featured-image.png";
 
@@ -12,15 +13,62 @@ class AddGroupDialog extends Component {
 
     state = {
         name: "",
-        photoUrl: null,
+        photoUrl: "",
+        formErrors: {
+            name: "",
+        }
     }
+
+    formValid = () => {
+        let valid = true;
+        const { formErrors, photoUrl, ...rest } = this.state;
+
+        Object.values(formErrors).forEach(val => {
+            val && val.length > 0 && (valid = false);
+        });
+
+        Object.values(rest).forEach(val => {
+            !val && (valid = false);
+        });
+
+
+        return valid;
+    };
+
+    handleChange = (name) => (event) => {
+        event.preventDefault();
+        const { value } = event.target;
+        const formErrors = { ...this.state.formErrors };
+
+        switch (name) {
+            case "name":
+                formErrors.name = value.length < 1 ? "Name is required" : "";
+                break;
+            default:
+                break;
+        }
+        this.setState({ formErrors, [name]: value });
+    };
+
+    submitForm = (e) => {
+        e.preventDefault();
+
+        if (!this.formValid()) {
+            return
+        }
+
+        this.props.onSubmit(this.state.name, this.state.photoUrl);
+        this.setState({name: "", photoUrl: ""});
+        this.props.onClose();
+
+    };
 
     render() {
         let {open, onSubmit, onClose} = this.props;
         const {name, photoUrl} = this.state;
         const setName = name => this.setState({name});
         const setPhotoUrl = photoUrl => this.setState({photoUrl});
-
+        const { formErrors } = this.state;
         return (
 
             <Dialog open={open}
@@ -38,9 +86,12 @@ class AddGroupDialog extends Component {
                         label="Nazwa grupy"
                         type="text"
                         fullWidth
-                        value={name}
-                        onChange={e => setName(e.target.value)}
+                        value={this.state.name}
+                        onChange={this.handleChange('name')}
                     />
+                    {formErrors.name.length > 0 && (
+                        <span className={css.error}>{formErrors.name}</span>
+                    )}
                     <TextField
                         margin="dense"
                         id="add-grup-photo"
@@ -56,12 +107,7 @@ class AddGroupDialog extends Component {
                     <Button onClick={onClose} color="primary" variant="text">
                         Cofnij
                     </Button>
-                    <Button onClick={() => {
-                        onSubmit(name, photoUrl);
-                        setName("");
-                        setPhotoUrl("");
-                        onClose();
-                    }} color="primary" variant="contained">
+                    <Button onClick={this.submitForm} color="primary" disabled={!this.formValid()} variant="contained">
                         Dodaj
                     </Button>
                 </DialogActions>
