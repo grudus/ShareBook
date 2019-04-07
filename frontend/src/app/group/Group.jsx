c83887ebf76fe3e3e37815635df589e0e6b3df37
grudus <jakgruda@gmail.com>
2019-04-07 22:18:32 +0200

    Mock temporarily groups

--- frontend/src/app/group/Group.jsx
+++ frontend/src/app/group/Group.jsx
@@ -8,7 +8,7 @@ import "./group.module.scss";
 import css from './group.module.scss';
 import Avatar from "./Avatar";
 import MenuList from "./Menu";
-import {Card} from "@material-ui/core";
+import { Card } from "@material-ui/core";
 
 class Group extends Component {
 
@@ -19,7 +19,31 @@ class Group extends Component {
 
 
     async componentDidMount() {
-        const groups = await GroupApi.getAllGroups();
+        // const groups = await GroupApi.getAllGroups();
+        const groups = [
+            {
+                id: 1,
+                name: "Grupa1",
+                photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa4tG8WJkzRsq8GsHtuxFtcpmXrp89GffQSYAkP-HQscRIR8IC8A"
+            },
+            {
+                id: 2,
+                name: "Grupa2",
+                photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa4tG8WJkzRsq8GsHtuxFtcpmXrp89GffQSYAkP-HQscRIR8IC8A"
+            },
+            {
+                id: 3,
+                name: "Grupa3",
+                photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa4tG8WJkzRsq8GsHtuxFtcpmXrp89GffQSYAkP-HQscRIR8IC8A"
+            },
+            {
+                id: 4,
+                name: "Grupa4",
+                photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa4tG8WJkzRsq8GsHtuxFtcpmXrp89GffQSYAkP-HQscRIR8IC8A"
+            },
+
+
+        ]
         this.setState({ groups })
     }
 
@@ -43,27 +67,27 @@ class Group extends Component {
     render() {
         return (
             <div>
-            <div className={css.topBar}>
-                <label className={css.title} width="100">Sharebook</label>
-                <MenuList/>
-                <Avatar/>
-            </div>
+                <div className={css.topBar}>
+                    <label className={css.title} width="100">Sharebook</label>
+                    <MenuList/>
+                    <Avatar/>
+                </div>
                 <div className={css.groups}>
-                Dostępne grupy:
-                <UserGroupList groups={this.state.groups}/>
+                    Dostępne grupy:
+                    <UserGroupList groups={this.state.groups}/>
                 </div>
                 <Card className={css.Card}></Card>
                 <div className={css.button}>
-                <AddGroupDialog
-                    open={this.state.showDialog}
-                    onClose={this.hideDialog}
-                    onSubmit={this.addGroup}
-                />
+                    <AddGroupDialog
+                        open={this.state.showDialog}
+                        onClose={this.hideDialog}
+                        onSubmit={this.addGroup}
+                    />
 
 
-                <Fab color={"primary"} onClick={this.showDialog}>
-                    <AddIcon/>
-                </Fab>
+                    <Fab color={"primary"} onClick={this.showDialog}>
+                        <AddIcon/>
+                    </Fab>
                 </div>
 
             </div>
