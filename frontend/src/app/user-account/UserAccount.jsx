a386cfafd5d9795c43f9596be723c1a6d1480f6d
grudus <jakgruda@gmail.com>
2019-06-03 19:56:55 +0200

    Show snackbar after edited user data

--- frontend/src/app/user-account/UserAccount.jsx
+++ frontend/src/app/user-account/UserAccount.jsx
@@ -1,17 +1,19 @@
+import Snackbar from "@material-ui/core/Snackbar";
 import css from './user-account.module.scss';
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import React, { Component } from "react";
-import {getCurrentUser} from "../user/UserApi";
+import { getCurrentUser } from "../user/UserApi";
 import LetterAvatar from "../group/LetterAvatar";
 import Card from "@material-ui/core/Card";
 import TextField from "@material-ui/core/TextField";
 import { Button } from "@material-ui/core";
-import {updateUser} from "../user/UserApi";
+import { updateUser } from "../user/UserApi";
 
 
 class UserAccount extends Component {
 
-    state = { user: null };
+    state = { user: null, showMessage: false };
+
     async componentDidMount() {
         const user = await getCurrentUser();
         this.setState({ user })
@@ -20,23 +22,24 @@ class UserAccount extends Component {
     submitForm = async (e) => {
         e.preventDefault();
         try {
-            await  updateUser(this.state.user);
+            await updateUser(this.state.user);
+            this.setState({showMessage: true})
         } catch (e) {
             alert("Invalid credentials")
         }
 
     };
 
-   updateFirstName = (event) => {
-       const newUsername = event.target.value;
-       this.setState(state => ({
-           ...state,
-           user: {
-               ...state.user,
-               firstName: newUsername,
-           }
-       }))
-   }
+    updateFirstName = (event) => {
+        const newUsername = event.target.value;
+        this.setState(state => ({
+            ...state,
+            user: {
+                ...state.user,
+                firstName: newUsername,
+            }
+        }))
+    }
 
     updateLastName = (event) => {
         const newUsername = event.target.value;
@@ -60,7 +63,7 @@ class UserAccount extends Component {
         }))
     }
 
-    render(){
+    render() {
         const { user } = this.state;
 
         if (!user) {
@@ -68,62 +71,75 @@ class UserAccount extends Component {
         }
 
 
-        return(
+        return (
             <Card className={css.accountWrapper}>
                 <div className={css.account}>
-            <div className={css.avatar}>
-                <LetterAvatar className={css.letterAvatar} text={user.firstName[0] + user.lastName[0]}/>
-            <h2>Witaj, {this.state.user.firstName}!</h2>
-            </div>
-                <div className={css.date}>
-                    <h3>Twoje dane: </h3>
-                <div className={css.fieldWrapper}>
-                    <div className={css.label}>
-                    <label>Imię: </label>
+                    <div className={css.avatar}>
+                        <LetterAvatar className={css.letterAvatar} text={user.firstName[0] + user.lastName[0]}/>
+                        <h2>Witaj, {this.state.user.firstName}!</h2>
                     </div>
-                    <TextField
-                        id="outlined-name"
-                        className={css.textField}
-                        value={this.state.user.firstName}
-                        onChange={this.updateFirstName}
-                        margin="normal"
-                        type="text"
-                        variant="outlined"
-                    />
-                </div>
-                <div className={css.fieldWrapper}>
-                    <div className={css.label}>
-                        <label>Nazwisko: </label>
+                    <div className={css.date}>
+                        <h3>Twoje dane: </h3>
+                        <div className={css.fieldWrapper}>
+                            <div className={css.label}>
+                                <label>Imię: </label>
+                            </div>
+                            <TextField
+                                id="outlined-name"
+                                className={css.textField}
+                                value={this.state.user.firstName}
+                                onChange={this.updateFirstName}
+                                margin="normal"
+                                type="text"
+                                variant="outlined"
+                            />
+                        </div>
+                        <div className={css.fieldWrapper}>
+                            <div className={css.label}>
+                                <label>Nazwisko: </label>
+                            </div>
+                            <TextField
+                                id="outlined-name"
+                                className={css.textField}
+                                value={this.state.user.lastName}
+                                onChange={this.updateLastName}
+                                margin="normal"
+                                type="text"
+                                variant="outlined"
+                            />
+                        </div>
+                        <div className={css.fieldWrapper}>
+                            <div className={css.label}>
+                                <label>Email: </label>
+                            </div>
+                            <TextField
+                                id="outlined-name"
+                                className={css.textField}
+                                value={this.state.user.email}
+                                onChange={this.updateEmail}
+                                margin="normal"
+                                type="text"
+                                variant="outlined"
+                            />
+                        </div>
+                        <br/><br/>
+                        <Button variant="outlined" color="primary" onClick={this.submitForm} type="submit">Edytuj
+                            dane</Button>
                     </div>
-                    <TextField
-                        id="outlined-name"
-                        className={css.textField}
-                        value={this.state.user.lastName}
-                        onChange={this.updateLastName}
-                        margin="normal"
-                        type="text"
-                        variant="outlined"
-                    />
                 </div>
-                <div className={css.fieldWrapper}>
-                    <div className={css.label}>
-                        <label>Email: </label>
-                    </div>
-                    <TextField
-                        id="outlined-name"
-                        className={css.textField}
-                        value={this.state.user.email}
-                        onChange={this.updateEmail}
-                        margin="normal"
-                        type="text"
-                        variant="outlined"
-                    />
-                </div><br/><br/>
-                    <Button variant="outlined" color="primary" onClick={this.submitForm} type="submit" >Edytuj dane</Button>
-            </div>
-                </div>
+
+
+                <Snackbar
+                    anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                    open={this.state.showMessage}
+                    onClose={() => this.setState({showMessage: false})}
+                    message={<span id="message-id">Twoje dane zostały zapisane!</span>}
+                    autoHideDuration={2000}
+                />
+
             </Card>
-        )}
+        )
+    }
 
 }
 
